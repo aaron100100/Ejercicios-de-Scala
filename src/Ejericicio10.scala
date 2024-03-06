6381b6a112bd55466e669dca0642bbc731baf8fd
Aarón Meza Ramos <aaronmr01@ciencias.unam.mx>
2024-03-05 19:41:22 -0600

    Agregando el ejercicio 10

---
+++ src/Ejericicio10.scala
@@ -0,0 +1,15 @@
+object Ejericicio10 extends   App {
+  println("Ingresa un entero: ")
+  val a = scala.io.StdIn.readInt()
+  println("Ingresa otro entero: ")
+  val b = scala.io.StdIn.readInt()
+
+  println("La suma de " + a  + " y " + b + " es: " + (a + b))
+  println("La resta de " + a  + " y " + b + " es: " + (a - b))
+  println("El producto de " + a  + " y " + b + " es: " + (a * b))
+  println("El cociente de " + a  + " y " + b + " es: " + (a / b))
+  println("El residuo de " + a  + " y " + b + " es: " + (a % b))
+  println("El Logaritmo de " + a  + " es: " + (math.log10(a)))
+  println("La potencia de " + a  + " a la  " + b + " es: " + (math.pow(a,b)))
+
+}
