2055edb598dc7f0e8898f3bba3abe2bccb8c424c
Aarón Meza Ramos <aaronmr01@ciencias.unam.mx>
2024-03-14 15:54:45 -0600

    Agregando menores o iguales a las condicionales

--- src/Ejercicio49.scala
+++ src/Ejercicio49.scala
@@ -6,25 +6,25 @@ object Ejercicio49 extends App {
   if(magnitud < 2.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría Microtemblor")
   }
-  else if ( magnitud > 2.0 && magnitud < 3.0){
+  else if ( magnitud >= 2.0 && magnitud < 3.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría muy menor")
   }
-  else if ( magnitud > 3.0 && magnitud < 4.0){
+  else if ( magnitud >= 3.0 && magnitud < 4.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoríA menor")
   }
-  else if ( magnitud > 4.0 && magnitud < 5.0){
+  else if ( magnitud >= 4.0 && magnitud < 5.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría ligero")
   }
-  else if ( magnitud > 5.0 && magnitud < 6.0){
+  else if ( magnitud >= 5.0 && magnitud < 6.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría moderado")
   }
-  else if ( magnitud > 6.0 && magnitud < 7.0){
+  else if ( magnitud >= 6.0 && magnitud < 7.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría fuerte")
   }
-  else if ( magnitud > 7.0 && magnitud < 8.0){
+  else if ( magnitud >= 7.0 && magnitud < 8.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría Importante")
   }
-  else if ( magnitud > 8.0 && magnitud < 10.0){
+  else if ( magnitud >= 8.0 && magnitud < 10.0){
     println("El sismo tiene magnitud " + magnitud + " y es considerado de categoría gran")
   }
   else{
